c675d43d677949cb70c078cd19a793e0ec4141c9
Jakub <kubanowak@gmail.com>
2025-03-31 12:58:41 +0200

    docs updates

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-#for PROJ in "core" "react-router-dom" "remix" "react-router"; do
-#  pnpm --filter @react-params/$PROJ exec pnpm version patch
-#  pnpm --filter @react-params/$PROJ build
-#  pnpm --filter @react-params/$PROJ publish --access public --no-git-checks
-#done
+for PROJ in "core" "react-router-dom" "remix" "react-router"; do
+  pnpm --filter @react-params/$PROJ exec pnpm version patch
+  pnpm --filter @react-params/$PROJ build
+  pnpm --filter @react-params/$PROJ publish --access public --no-git-checks
+done
 
-#for PROJ in "utils" "vanilla" "react-router-dom-v6" "react-router-dom-v7" "remix" "react-router" ; do
-for PROJ in  "remix"  ; do
+for PROJ in "utils" "vanilla" "react-router-dom-v6" "react-router-dom-v7" "remix" "react-router" ; do
   pnpm  --filter @react-params/test-$PROJ update @react-params/core@latest @react-params/react-router-dom@latest @react-params/remix@latest @react-params/react-router@latest
   find ./test/$PROJ -name "node_modules" -type d -prune -exec rm -rf {} +
   pnpm --filter @react-params/test-$PROJ install
